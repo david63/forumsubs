2888be4a33ef14a40565b8f370dc87cf7aa4806c
David Wood <david.wood@pear-tree.org.uk>
2021-01-22 11:18:21 +0000

    Update

--- adm/style/js/donate-collapse.js
+++ adm/style/js/donate-collapse.js
@@ -1,4 +1,4 @@
-;(function($, document)
+;(function($)
 {
 	'use strict';
 
--- adm/style/js/forumcat.js
+++ adm/style/js/forumcat.js
@@ -1,16 +1,18 @@
-;(function($, document)
+;(function($)
 {
 	'use strict';
 
 	$('tr.forum-cat').click(function(){
-    	$(this).nextUntil('tr.forum-cat').css('display', function(i, v){
-        return this.style.display === 'table-row' ? 'none' : 'table-row';
+    	$(this).nextUntil('tr.forum-cat').css('display', function()
+		{
+        	return this.style.display === 'table-row' ? 'none' : 'table-row';
     	});
 	});
 
 	$('div.forum-cat').click(function(){
-    	$(this).nextUntil('div.forum-cat').css('display', function(i, v){
-        return this.style.display === 'table-row' ? 'none' : 'table-row';
+    	$(this).nextUntil('div.forum-cat').css('display', function()
+		{
+        	return this.style.display === 'table-row' ? 'none' : 'table-row';
     	});
 	});
 })(jQuery, document);
